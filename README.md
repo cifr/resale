3d8bf52eb215c308f7e42e1a43b13b260fea9cfc
Bernard yap <b@lofi0.net>
2016-09-21 11:11:44 +0800

    README & disclaimers

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# Resale
+### Resale Flat Prices
 
-foobar
+A simple linear model for estimating resale flat prices in Singapore based on historical transaction data from Data.gov.sg. 
+[Resale flats](http://www.hdb.gov.sg/cs/infoweb/residential/buying-a-flat/resale) are pre-owned public housing in
+Singapore availble for purchase in the open market. 
+
+* Access the app at: [https://cifr.shinyapps.io/resale/](https://cifr.shinyapps.io/resale/)
+
+-----
+
+**Disclaimers:** *This prediction model built based on historical data from Resale Flat Prices (data.gov.sg) shall not be attributed to the Singapore Government or its Statutory Boards; The datasets provided by the Singapore Government and its Statutory Boards via Data.gov.sg are governed by the Terms of Use available at https://data.gov.sg/terms . To the fullest extent permitted by law, the Singapore Government and its Statutory Boards are not liable for any damage or loss of any kind caused directly or indirectly by the use of the datasets or any derived analyses or applications.*
--- ui.R
+++ ui.R
@@ -3,25 +3,31 @@ source("resale.R")
 
 shinyUI(fluidPage(
     titlePanel("Price of Resale Flats in Singapore"),
-    sidebarPanel(
-        selectInput("flat_type", "Flat Type", ddp.flat_types,
-                    selected=ddp.default$flat_type),
-        selectInput("town", "Town", ddp.towns,
-                    selected=ddp.default$town),
-        sliderInput("floor_area_sqm", "Floor Area (SQM)", ddp.default$floor_area_sqm,
-                     min=ddp.min_floor_area, max=ddp.max_floor_area, step=1),
-        sliderInput("age_year", "Flat Age (Y)", ddp.default$age_year,
-                     min=5, max=99, step=1),
-        br(),
-        p("Prediction model based on historical data from ",
-          a("Resale Flat Prices",
-            href="https://data.gov.sg/dataset/resale-flat-prices"),
-          "(data.gov.sg)"),
-        br()
-    ),
-    mainPanel(
-        h3(textOutput("resale_price")),
-        textOutput("message"),
-        plotOutput("resale_chart")
-    )
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("flat_type", "Flat Type", ddp.flat_types,
+                        selected=ddp.default$flat_type),
+            selectInput("town", "Town", ddp.towns,
+                        selected=ddp.default$town),
+            sliderInput("floor_area_sqm", "Floor Area (SQM)", ddp.default$floor_area_sqm,
+                         min=ddp.min_floor_area, max=ddp.max_floor_area, step=1),
+            sliderInput("age_year", "Flat Age (Y)", ddp.default$age_year,
+                         min=5, max=99, step=1),
+            br(), br(), p(a("https://github.com/cifr/resale", href="https://github.com/cifr/resale"))
+        ),
+        mainPanel(
+            h3(textOutput("resale_price")),
+            textOutput("message"),
+            plotOutput("resale_chart")
+        )),
+    hr(),
+    p(class="small", "This prediction model built based on historical data from ",
+      a("Resale Flat Prices", href="https://data.gov.sg/dataset/resale-flat-prices"),
+      "(data.gov.sg) shall not be attributed to the Singapore Government or its Statutory Boards;",
+      "The datasets provided by the Singapore Government and its Statutory Boards via Data.gov.sg",
+      "are governed by the Terms of Use available at ", 
+      a("https://data.gov.sg/terms", href="https://data.gov.sg/terms"), 
+      ". To the fullest extent permitted by law, the Singapore Government and its Statutory Boards",
+      "are not liable for any damage or loss of any kind caused directly or indirectly by the use",
+      "of the datasets or any derived analyses or applications.")
 ))
